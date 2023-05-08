d84ce972ef55319d0fd7ed40b09924fde52e107d
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-08 22:53:07 +0200

    do not put installer in C:\RCO2, more
    change urls, download needed dlls
    
    Signed-off-by: Tanki <100938045+fheahdythdr@users.noreply.github.com>

--- RCO2Installer/RCO2Installer/main.cpp
+++ RCO2Installer/RCO2Installer/main.cpp
@@ -2,13 +2,14 @@
 #include <filesystem>
 #include <thread>
 #include <curl/curl.h>
+#include <windows.h>
 
 int main(int argc, char* argv[]) {
 	std::string userIn;
 	std::cout << "Welcome to the RCO Installer/Updater!\nRCO is made, owned, and developed by Kaede and L8X\n\nType \"install\" and press enter to continue!\nType \"uninstall\" and press enter to uninstall :(\n\n";
 	std::cin >> userIn;
 
-    std::string rootDir("C:\\Program Files (x86)\\RCO2");
+    std::string rootDir("C:\\RCO2");
 
     if (std::filesystem::exists(rootDir) == false) {
         std::filesystem::create_directory(rootDir);
@@ -24,7 +25,7 @@ int main(int argc, char* argv[]) {
 
         CURL* req = curl_easy_init();
         CURLcode res;
-        curl_easy_setopt(req, CURLOPT_URL, "https://roblox-client-optimizer.simulhost.com/RCO.exe");
+        curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/RCO.exe");
         curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
         curl_easy_setopt(req, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
         curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
@@ -39,16 +40,41 @@ int main(int argc, char* argv[]) {
 
         fclose(file);
 
-        if (std::filesystem::exists(rootDir + "\\RCO2Installer.exe") == false) {
-        FILE* file2;
-        if (fopen_s(&file2, (rootDir + "\\RCO2Installer.exe").c_str(), "wb") != 0) {
-            std::cout << "Could not open filepoint... | 0x3\n";
-            std::cin.get();
-            return 3;
-        }
+        if (std::filesystem::exists(rootDir + "\\libcurl.dll") == false) {
+            FILE* file2;
+            if (fopen_s(&file2, (rootDir + "\\libcurl.dll").c_str(), "wb") != 0) {
+                std::cout << "Could not open filepoint... | 0x3\n";
+                std::cin.get();
+                return 3;
+            }
             CURL* req2 = curl_easy_init();
             CURLcode res2;
-            curl_easy_setopt(req2, CURLOPT_URL, "https://roblox-client-optimizer.simulhost.com/RCO2Installer.exe");
+            curl_easy_setopt(req2, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/libcurl.dll");
+            curl_easy_setopt(req2, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            curl_easy_setopt(req2, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            curl_easy_setopt(req2, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(req2, CURLOPT_WRITEDATA, file2);
+            res2 = curl_easy_perform(req2);
+            if (res2 != CURLE_OK) {
+                std::cout << "NETWORK ERROR | PLEASE CHECK YOUR INTERNET CONNECTION | 0x4\n";
+                std::cin.get();
+                return 4;
+            }
+            curl_easy_cleanup(req2);
+
+            fclose(file2);
+        }
+
+        if (std::filesystem::exists(rootDir + "\\zlib1.dll") == false) {
+            FILE* file2;
+            if (fopen_s(&file2, (rootDir + "\\zlib1.dll").c_str(), "wb") != 0) {
+                std::cout << "Could not open filepoint... | 0x3\n";
+                std::cin.get();
+                return 3;
+            }
+            CURL* req2 = curl_easy_init();
+            CURLcode res2;
+            curl_easy_setopt(req2, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/zlib1.dll");
             curl_easy_setopt(req2, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
             curl_easy_setopt(req2, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
             curl_easy_setopt(req2, CURLOPT_WRITEFUNCTION, NULL);
@@ -89,14 +115,14 @@ int main(int argc, char* argv[]) {
         std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	}
     else if (userIn == std::string("uninstall")) { // Uninstall
-        if (argv[0] == (rootDir+"\\RCO2Installer.exe").c_str() || argv[0] == "C:\\RClientOptimizer2\\RCO2.exe") {
+        if (argv[0] == (rootDir+"\\RCO2Installer.exe").c_str() || argv[0] == "C:\\RCO2\\RCO2.exe") {
             std::cout << "To uninstall, please run the installer outside of this updater, you can redownload this installer on the github.\nClosing in 5 seconds.\n";
             std::this_thread::sleep_for(std::chrono::milliseconds(5000));
             return 0;
         }
 
-        if (std::filesystem::exists("C:\\RClientOptimizer2") == true) {
-            std::filesystem::remove_all("C:\\RClientOptimizer2");
+        if (std::filesystem::exists("C:\\RCO2") == true) {
+            std::filesystem::remove_all("C:\\RCO2");
         }
 
         if (std::filesystem::exists(rootDir) == true) {
