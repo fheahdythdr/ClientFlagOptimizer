c05c656876aaf8e9ceae88f80d9191ea6c113db2
Tanki <100938045+fheahdythdr@users.noreply.github.com>
2023-05-10 16:06:34 +0200

    add updater
    Signed-off-by: Tanki <100938045+fheahdythdr@users.noreply.github.com>

--- RCO2Installer/RCO2Installer/main.cpp
+++ RCO2Installer/RCO2Installer/main.cpp
@@ -10,7 +10,11 @@ int main(int argc, char* argv[]) {
 	std::cin >> userIn;
 
     std::string rootDir("C:\\RCO2");
+    std::string updDir("C:\\RCO2Updater");
 
+    if (std::filesystem::exists(updDir) == false) {
+        std::filesystem::create_directory(updDir);
+    }
     if (std::filesystem::exists(rootDir) == false) {
         std::filesystem::create_directory(rootDir);
     }
@@ -41,53 +45,128 @@ int main(int argc, char* argv[]) {
         fclose(file);
 
         if (std::filesystem::exists(rootDir + "\\libcurl.dll") == false) {
-            FILE* file2;
-            if (fopen_s(&file2, (rootDir + "\\libcurl.dll").c_str(), "wb") != 0) {
+            FILE* file;
+            if (fopen_s(&file, (rootDir + "\\libcurl.dll").c_str(), "wb") != 0) {
                 std::cout << "Could not open filepoint... | 0x3\n";
                 std::cin.get();
-                return 3;
+                return 1;
             }
-            CURL* req2 = curl_easy_init();
-            CURLcode res2;
-            curl_easy_setopt(req2, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/libcurl.dll");
-            curl_easy_setopt(req2, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
-            curl_easy_setopt(req2, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-            curl_easy_setopt(req2, CURLOPT_WRITEFUNCTION, NULL);
-            curl_easy_setopt(req2, CURLOPT_WRITEDATA, file2);
-            res2 = curl_easy_perform(req2);
-            if (res2 != CURLE_OK) {
+            CURL* req = curl_easy_init();
+            CURLcode res;
+            curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/libcurl.dll");
+            curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            curl_easy_setopt(req, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(req, CURLOPT_WRITEDATA, file);
+            res = curl_easy_perform(req);
+            if (res != CURLE_OK) {
                 std::cout << "NETWORK ERROR | PLEASE CHECK YOUR INTERNET CONNECTION | 0x4\n";
                 std::cin.get();
-                return 4;
+                return 2;
             }
-            curl_easy_cleanup(req2);
+            curl_easy_cleanup(req);
 
-            fclose(file2);
+            fclose(file);
+        }
+
+        if (std::filesystem::exists(updDir + "\\libcurl.dll") == false) {
+            FILE* file;
+            if (fopen_s(&file, (updDir + "\\libcurl.dll").c_str(), "wb") != 0) {
+                std::cout << "Could not open filepoint... | 0x3\n";
+                std::cin.get();
+                return 1;
+            }
+            CURL* req = curl_easy_init();
+            CURLcode res;
+            curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/RCO2Updater/libcurl.dll");
+            curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            curl_easy_setopt(req, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(req, CURLOPT_WRITEDATA, file);
+            res = curl_easy_perform(req);
+            if (res != CURLE_OK) {
+                std::cout << "NETWORK ERROR | PLEASE CHECK YOUR INTERNET CONNECTION | 0x4\n";
+                std::cin.get();
+                return 2;
+            }
+            curl_easy_cleanup(req);
+
+            fclose(file);
+        }
+
+        if (std::filesystem::exists(updDir + "\\zlib1.dll") == false) {
+            FILE* file;
+            if (fopen_s(&file, (updDir + "\\zlib1.dll").c_str(), "wb") != 0) {
+                std::cout << "Could not open filepoint... | 0x3\n";
+                std::cin.get();
+                return 1;
+            }
+            CURL* req = curl_easy_init();
+            CURLcode res;
+            curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/RCO2Updater/zlib1.dll");
+            curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            curl_easy_setopt(req, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(req, CURLOPT_WRITEDATA, file);
+            res = curl_easy_perform(req);
+            if (res != CURLE_OK) {
+                std::cout << "NETWORK ERROR | PLEASE CHECK YOUR INTERNET CONNECTION | 0x4\n";
+                std::cin.get();
+                return 2;
+            }
+            curl_easy_cleanup(req);
+
+            fclose(file);
+        }
+
+        if (std::filesystem::exists(updDir + "\\RCO-Updater.exe") == false) {
+            FILE* file;
+            if (fopen_s(&file, (updDir + "\\RCO-Updater.exe").c_str(), "wb") != 0) {
+                std::cout << "Could not open filepoint... | 0x3\n";
+                std::cin.get();
+                return 1;
+            }
+            CURL* req = curl_easy_init();
+            CURLcode res;
+            curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/RCO2Updater.exe");
+            curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            curl_easy_setopt(req, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(req, CURLOPT_WRITEDATA, file);
+            res = curl_easy_perform(req);
+            if (res != CURLE_OK) {
+                std::cout << "NETWORK ERROR | PLEASE CHECK YOUR INTERNET CONNECTION | 0x4\n";
+                std::cin.get();
+                return 2;
+            }
+            curl_easy_cleanup(req);
+
+            fclose(file);
         }
 
         if (std::filesystem::exists(rootDir + "\\zlib1.dll") == false) {
-            FILE* file2;
-            if (fopen_s(&file2, (rootDir + "\\zlib1.dll").c_str(), "wb") != 0) {
+            FILE* file;
+            if (fopen_s(&file, (rootDir + "\\zlib1.dll").c_str(), "wb") != 0) {
                 std::cout << "Could not open filepoint... | 0x3\n";
                 std::cin.get();
-                return 3;
+                return 1;
             }
-            CURL* req2 = curl_easy_init();
-            CURLcode res2;
-            curl_easy_setopt(req2, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/zlib1.dll");
-            curl_easy_setopt(req2, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
-            curl_easy_setopt(req2, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-            curl_easy_setopt(req2, CURLOPT_WRITEFUNCTION, NULL);
-            curl_easy_setopt(req2, CURLOPT_WRITEDATA, file2);
-            res2 = curl_easy_perform(req2);
-            if (res2 != CURLE_OK) {
+            CURL* req = curl_easy_init();
+            CURLcode res;
+            curl_easy_setopt(req, CURLOPT_URL, "https://raw.githubusercontent.com/fheahdythdr/rco-but-it-uses-different-fflags/main/zlib1.dll");
+            curl_easy_setopt(req, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2TLS);
+            curl_easy_setopt(req, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+            curl_easy_setopt(req, CURLOPT_WRITEFUNCTION, NULL);
+            curl_easy_setopt(req, CURLOPT_WRITEDATA, file);
+            res = curl_easy_perform(req);
+            if (res != CURLE_OK) {
                 std::cout << "NETWORK ERROR | PLEASE CHECK YOUR INTERNET CONNECTION | 0x4\n";
                 std::cin.get();
-                return 4;
+                return 2;
             }
-            curl_easy_cleanup(req2);
+            curl_easy_cleanup(req);
 
-            fclose(file2);
+            fclose(file);
         }
 
         HKEY hKey;
@@ -106,7 +185,6 @@ int main(int argc, char* argv[]) {
         ZeroMemory(&si, sizeof(si));
         si.cb = sizeof(si);
         ZeroMemory(&pi, sizeof(pi));
-
         CreateProcessA((rootDir + "\\RCO.exe").c_str(), argv[1], NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
         CloseHandle(pi.hProcess);
         CloseHandle(pi.hThread);
